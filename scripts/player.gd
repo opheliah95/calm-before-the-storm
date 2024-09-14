c4011c959396607a37ab3f037768bea5e7bb529f
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-09-15 00:36:46 +0100

    some initial animation setup

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,10 @@ const MAX_SPEED = 150;
 const ACCELERATION=100;
 const DECELERATION = 500;
 
+var animationPlayer = null;
+
 func _ready():
+	animationPlayer = $PlayerAnimationPlayer
 	pass
 
 func get_input() :
@@ -17,8 +20,14 @@ func _physics_process(delta: float):
 	if direction != Vector2.ZERO:
 		velocity += direction * ACCELERATION 
 		velocity =  velocity.normalized() * MAX_SPEED 
+		%Player_Idle.visible = false
+		%Player_Run_Front.visible = true
+		%PlayerAnimationPlayer.play("run-front")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+		%Player_Idle.visible = true
+		%Player_Run_Front.visible = false
+		%PlayerAnimationPlayer.play("idle-front")
 	#print("the velocity is: ", velocity)
 		
 	move_and_slide()
