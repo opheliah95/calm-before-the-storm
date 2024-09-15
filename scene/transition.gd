e2fab02c1c6ac47efe855beaf6109e6944267fc8
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-09-15 04:40:51 +0100

    transition will pause game logic

--- scene/transition.gd
+++ scene/transition.gd
@@ -2,12 +2,15 @@ extends CanvasLayer
 
 func _on_ready():
 	%FadeScreen.visible = false
-	%FadeScreenAnimPlayer.animation_finished.connect(_on_animation_finished)
 
 func transition():
 	%FadeScreen.visible = true
 	%FadeScreenAnimPlayer.play("fade_out")
+	get_tree().paused = true
 
-func _on_animation_finished():
-	%FadeScreen.visible = false
+	  # Print to confirm the function runs
+func _on_fade_screen_anim_player_animation_finished(anim_name: StringName) -> void:
+	print("Animation finished")
+	$FadeScreen.visible = false
+	get_tree().paused = false  # Resume the game when the animation is done
 	
